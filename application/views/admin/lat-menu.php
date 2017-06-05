530a3c02271095d1458d803fee222f0bd36eadcd
Ivan Mera <imera92@gmail.com>
2017-06-05 12:58:07 -0500

    Menu lateral del ADMIN

---
+++ application/views/admin/lat-menu.php
@@ -0,0 +1,28 @@
+	<div id="wrapper">
+		<!-- Sidebar -->
+		<div id="sidebar-wrapper">
+		    <ul class="sidebar-nav">
+		        <li class="sidebar-brand">
+		            <a href="#">
+		                <img class="img-responsive" src="<?php echo base_url('public/img/logo.jpg'); ?>">
+		            </a>
+		        </li>
+		        <li>
+		            <a href="#collapse1" data-toggle="collapse" ><i class="fa fa-cube" aria-hidden="true"></i> Productos</a>
+		        </li>
+	            <div id="collapse1" class="panel-collapse collapse">
+	                <ul class="sidebar-subnav list-group">
+	                    <li>
+	                    	<a href="#"><i class="fa fa-plus" aria-hidden="true"></i> Crear Producto</a>
+	                    </li>
+	                    <li>
+	                    	<a href="#"><i class="fa fa-file-excel-o" aria-hidden="true"></i> Actualizar Catálogo</a>
+	                    </li>
+	                </ul>
+                </div>
+		        <li>
+		            <a href="<?php echo base_url('admin/logout'); ?>"><i class="fa fa-sign-out" aria-hidden="true"></i> Log out</a>
+		        </li>
+		    </ul>
+		</div>
+		<!-- /#sidebar-wrapper -->
