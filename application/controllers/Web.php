fda26183ba38b2bae0d58ac48bf7cbc5a594b020
Ivan Mera <imera92@gmail.com>
2017-06-08 06:28:00 -0500

    Vista provisional para carrito

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -8,6 +8,7 @@ class Web extends CI_Controller {
       $this->load->database();
       $this->load->helper('url');
       $this->load->helper('form');
+      $this->load->library('session');
       date_default_timezone_set("America/Guayaquil");
 	}
 
@@ -21,4 +22,24 @@ class Web extends CI_Controller {
     $this->load->view('web/footer');
   }
 
+  public function carrito()
+  {
+    $titulo = "Dimquality::Admin - Shopping Cart";
+    $dataHeader['titlePage'] = $titulo;
+
+    /*$user = $this->db->get_where('usuario', array('id' => 6))->result_array();
+    $this->session->set_userdata(array('id' => $user[0]['id'], 'user' => $user[0]['user']));
+
+    $carrito = $this->db->get_where('carrito', array('usuario' => $user[0]['id']))->result_array();
+    $dataBody['subtotal'] = $carrito[0]['subtotal'];
+    $productos = $this->db->get_where('productocarrito', array('carrito' => $carrito[0]['id']))->result_array();
+    print_r($productos);
+    die();
+
+    $dataBody['productosCarrito'] = $productos;*/
+
+    $this->load->view('web/header', $dataHeader);
+    $this->load->view('web/carrito');
+    $this->load->view('web/footer');
+  }
 }
