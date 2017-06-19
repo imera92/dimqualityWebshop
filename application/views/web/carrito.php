80a6fffde08f8a43a5c646ae583cd145ac99b6d8
Ivan Mera <imera92@gmail.com>
2017-06-19 15:20:24 -0500

    Reorganizacion de archivos

---
+++ application/views/web/carrito.php
@@ -0,0 +1,77 @@
+		<div class="row">
+			<h1>CARRITO DE COMPRAS</h1>
+		</div>
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 well">
+				<div class="row">
+					<div class="col-md-2">
+						<img class="img-responsive" src="http://via.placeholder.com/600x600">
+					</div>
+					<div class="col-md-5">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam augue, malesuada quis eros ac, aliquam pellentesque arcu. Pellentesque convallis commodo ante a placerat. Sed euismod ligula eu aliquam tempor. Sed arcu ex, vestibulum nec metus vitae, pharetra imperdiet massa. Curabitur nec tempus ligula, et blandit odio.
+						</p>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								PRECIO
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								$ 100
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								CANTIDAD
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<input type="number" step="1" value="1">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-10 col-md-offset-1 well">
+				<div class="row">
+					<div class="col-md-2">
+						<img class="img-responsive" src="http://via.placeholder.com/600x600">
+					</div>
+					<div class="col-md-5">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam augue, malesuada quis eros ac, aliquam pellentesque arcu. Pellentesque convallis commodo ante a placerat. Sed euismod ligula eu aliquam tempor. Sed arcu ex, vestibulum nec metus vitae, pharetra imperdiet massa. Curabitur nec tempus ligula, et blandit odio.
+						</p>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								PRECIO
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								$ 100
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								CANTIDAD
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<input type="number" step="1" value="1">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
