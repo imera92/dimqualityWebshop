7b8616c6804a828bcf901d6c15372e41725eb977
Washington Vélez <wjvelez@espol.edu.ec>
2017-06-21 11:45:03 -0500

    enlazando login de usuario

--- application/models/ShopUser.php
+++ application/models/ShopUser.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 	if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 	class ShopUser extends CI_Model{
 		var $persona = "";
         var $correo = "";
-        
+
         function __construct() {
             parent::__construct();
             $this->load->database();
@@ -36,7 +36,7 @@
         	$this->session->sess_destroy();
         }
 
-        public function insert($data = array()) {        
+        public function insert($data = array()) {
 
         //insert user data to users table
             $insert = $this->db->insert($this->userTbl, $data);
@@ -52,14 +52,14 @@
         function getRows($params = array()){
             $this->db->select('*');
             $this->db->from($this->userTbl);
-            
+
         //fetch data by conditions
             if(array_key_exists("conditions",$params)){
                 foreach ($params['conditions'] as $key => $value) {
                     $this->db->where($key,$value);
                 }
             }
-            
+
             if(array_key_exists("id",$params)){
                 $this->db->where('id',$params['id']);
                 $query = $this->db->get();
@@ -84,6 +84,21 @@
         //return fetched data
             return $result;
         }
+
+		function securityCheckUser() {
+	        $securityUser = new ShopUser();
+	        $usuario = $this->session->userdata('user');
+	        if($usuario == ""){
+	            return false;
+	        }else{
+	            if ($this->session->userdata('tipo') == "user") {
+	                return true;
+	            }else{
+	                $securityUser->logout();
+	                return false;
+	            }
+	        }
+	    }
 
 	}
 ?>
