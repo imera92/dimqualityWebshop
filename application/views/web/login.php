8acec69d4dc02bedf19b171a08feabd065e11db0
Ivan Mera <imera92@gmail.com>
2017-06-21 07:46:16 -0500

    Mejorando formulario de login

--- application/views/web/login.php
+++ application/views/web/login.php
@@ -7,17 +7,31 @@
 	            		<h3>Iniciar sesión con una cuenta de usuario</h3>
 	            		<hr class="secondary-separator">
 	            		<p>Si usted ya tiene una cuenta con nosotros, por favor ingrese sus credenciales</p>
-	            		<form>
-	            			<div class="form-group">
-	            			<label for="exampleInputEmail1">Dirección de correo electrónico<span class="red">*</span></label>
-	            				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-	            			</div>
-	            			<div class="form-group">
-	            				<label for="exampleInputPassword1">Contraseña<span class="red">*</span></label>
-	            				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-	            			</div>
-	            			<button type="submit" class="btn btn-login">Iniciar sesión</button><span class="required">* Campo Requerido</span>
-	            		</form>
+	            		<?php echo form_open('usuario/auth' , array('id' => 'frm-login')); ?>
+	            			<fieldset>
+		            			<div class="form-group">
+		            			<label for="userInput">Usuario<span class="red">*</span></label>
+		            				<?php echo form_input(array(
+		                                'id' => 'userInput',
+		                                'name' => 'user',
+		                                'value' => '',
+		                                'placeholder' => 'Ingrese su usuario',
+		                                'class' => 'form-control',
+		                            ));?>
+		            			</div>
+		            			<div class="form-group">
+		            				<label for="passwordInput">Contraseña<span class="red">*</span></label>
+		            				<?php echo form_password(array(
+		            					'id' => 'passwordInput',
+		                                'name' => 'password',
+		                                'value' => '',
+		                                'placeholder' => 'Ingrese su contraseña',
+		                                'class' => 'form-control',
+		                            ));?>
+		            			</div>
+		            			<button type="submit" class="btn btn-login">Iniciar sesión</button><span class="required">* Campo Requerido</span>
+	            			</fieldset>
+	            		<?php echo form_close(); ?>
 	            	</div>
 	            	<div class="col-xs-6">
 	            		<h3>Crear una cuenta de usuario</h3>
