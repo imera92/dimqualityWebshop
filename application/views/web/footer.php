2f4f071bd8bd420416260db5070076d08d5c5047
Juan Alvarado <juaelalv@espol.edu.ec>
2017-07-01 12:41:26 -0500

    Validacion usando jqueryValidate

--- application/views/web/footer.php
+++ application/views/web/footer.php
@@ -26,6 +26,7 @@
 	<script  src="http://code.jquery.com/jquery-1.11.1.min.js"  integrity="sha256-VAvG3sHdS5LqTT+5A/aeq/bZGa/Uj04xKxY8KM/w9EE=" crossorigin="anonymous"></script>
     <script src="<?php echo base_url('public/js/bootstrap.min.js'); ?>"></script>
 	<?php if ($this->router->method == 'crearUsuario'): ?>
+		<script src="<?php echo base_url('public/js/jquery.validate.min.js'); ?>"></script>
 		<script src="<?php echo base_url('public/js/crearUsuario.js'); ?>"></script>
 	<?php endif; ?>
 </body>
