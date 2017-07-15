15b815a9503247a42e734528aa3ef876e58b4686
Juan Alvarado <juaelalv@espol.edu.ec>
2017-07-15 12:40:54 -0500

    Sprint 3 task 85 Creacion de vista ActualizarUsuario
    Creacion de vista para Actualizar datos de Usuario

---
+++ application/views/web/actualizarUsuario.php
@@ -0,0 +1,59 @@
+    <div class="row mt-50 mb-50">
+      <div class="col-xs-10 col-xs-offset-1">
+        <h1>Crear Usuario</h1>
+        <hr>
+        <form id="userForm" method="POST">
+          <h4>Formulario de Registro</h4>
+          <hr class="secondary-separator">
+          <div class="row">
+            <div class="form-group col-md-6">
+              <label for="usuario">Usuario </label>
+              <input type="text" name="usuario" class="form-control" required placeholder="Ingrese su usuario" value="<?php echo !empty($user['user'])?$user['user']:''; ?>" />
+              <?php echo form_error('usuario','<span class="help-block">','</span>'); ?>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="nombre">Nombre </label>
+              <input type="text" name="nombre" class="form-control" required placeholder="Ingrese su Nombre" value="<?php echo !empty($user['nombre'])?$user['nombre']:''; ?>"/>
+              <?php echo form_error('nombre','<span class="help-block">','</span>'); ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="form-group col-md-6">
+              <label for="email">Correo Electrónico</label>
+              <input type="email" name="correo" class="form-control" required aria-describedby="emailHelp" placeholder="Ingrese su correo" value="<?php echo !empty($user['email'])?$user['email']:''; ?>"/>
+              <?php echo form_error('correo','<span class="help-block">','</span>'); ?>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="apellido">Apellido </label>
+              <input type="text" name="apellido" class="form-control" required placeholder="Ingrese su Apellido" value="<?php echo !empty($user['apellido'])?$user['apellido']:''; ?>"/>
+              <?php echo form_error('apellido','<span class="help-block">','</span>'); ?>
+            </div>
+          </div>
+          <h4>Información Adicional</h4>
+          <hr class="secondary-separator">
+          <div class="row">
+            <div class="form-group col-md-6">
+              <label for="cedula">Cédula o RUC</label>
+              <input type="text" class="form-control" name="cedula" maxlength="13" required placeholder="Ingrese su Nro. de Cedula"  value="<?php echo !empty($user['cedula'])?$user['cedula']:''; ?>">
+              <?php echo form_error('cedula','<span class="help-block">','</span>'); ?>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="telefono">Número de Teléfono</label>
+              <input type="text" class="form-control" name="telefono" maxlength="10" required placeholder="Ingrese número de teléfono" pattern="[0-9]{9,10}" value="<?php echo !empty($user['telefono'])?$user['telefono']:''; ?>">
+              <?php echo form_error('telefono','<span class="help-block">','</span>'); ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="form-group col-md-6">
+              <label for="direccion">Dirección </label>
+              <input type="text" class="form-control" name="direccion" required placeholder="Ingrese su dirección" value="<?php echo !empty($user['direccion'])?$user['direccion']:''; ?>">
+              <?php echo form_error('direccion','<span class="help-block">','</span>'); ?>
+            </div>
+          </div>
+          <div class="form-group col-md-12">
+            <input class="btn btn-registrar" name="submit" type="submit" value="Registrar"/>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
---
+++ public/js/actualizarUsuario.js
@@ -0,0 +1,131 @@
+$('#userForm').validate({
+    debug: false,
+    rules:{
+        conf_clave: {
+            equalTo: '#clave'
+        },
+        telefono:{
+            digits: true,
+            minlength: 7,
+            maxlength: 10
+        },
+        cedula:{
+            digits: true,
+            minlength: 10,
+            maxlength: 13
+        }
+    },
+    messages:{
+        usuario: 'Ingrese un usuario',
+        nombre: 'Ingrese un nombre',
+        apellido: 'Ingrese un apellido',
+        correo: {
+            required:'Ingrese un correo',
+            email: 'Ingrese un correo válido'
+        },
+        clave: 'Ingrese su contraseña',
+        conf_clave: {
+            required: 'Confirme su contraseña',
+            equalTo: 'La contraseña no coincide'
+        },
+        cedula: 'Ingrese una cedula o RUC válido',
+        telefono: {
+            required:'Ingrese un télefono',
+            digits: 'Ingrese un télefono válido'
+        },
+        direccion: 'Ingrese su dirección'
+    }
+});
+
+$(document).ready(function() {
+    $("input[name='usuario']").keydown(function (e) {
+        // Permitir: backspace, delete, tab, escape, enter, guion y .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 173, 190]) !== -1 ||
+            // Permitir: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+F5
+            (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: F5
+            (e.keyCode == 116) ||
+             // Permitir: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // No hacer nada, dejar que las teclas puedan se presionadas
+                 return;
+        }
+        if (e.shiftKey || ((e.keyCode < 48 || e.keyCode > 90) && (e.keyCode < 96 || e.keyCode > 105)) || e.keyCode == 61) {
+            e.preventDefault();
+        }
+    });
+    $("input[name='nombre'], input[name='apellido']").keydown(function (e) {
+        // Permitir: backspace, delete, tab, escape, enter
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+            // Permitir: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+F5
+            (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: F5
+            (e.keyCode == 116) ||
+             // Permitir: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39) ||
+            // Permitir: ñ
+            (e.keyCode == 0)) {
+                 // No hacer nada, dejar que las teclas puedan se presionadas
+                 return;
+        }
+        if (e.keyCode < 65 || e.keyCode > 90 || e.keyCode == 61) {
+            e.preventDefault();
+        }
+    });
+    $("input[name='cedula'], input[name='telefono']").keydown(function (e) {
+        // Permitir: backspace, delete, tab, escape, enter
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+            // Permitir: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+F5
+            (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: F5
+            (e.keyCode == 116) ||
+             // Permitir: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // No hacer nada, dejar que las teclas puedan se presionadas
+                 return;
+        }
+        if (e.shiftKey || ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) || e.keyCode == 61) {
+            e.preventDefault();
+        }
+    });
+    $("input[name='direccion']").keydown(function (e) {
+        // Permitir: space, backspace, delete, tab, escape, enter, guion y .
+        if ($.inArray(e.keyCode, [32, 46, 8, 9, 27, 13, 110, 173, 190]) !== -1 ||
+            // Permitir: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+F5
+            (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: F5
+            (e.keyCode == 116) ||
+             // Permitir: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // No hacer nada, dejar que las teclas puedan se presionadas
+                 return;
+        }
+        if (((e.keyCode < 48 || e.keyCode > 90) && (e.keyCode < 96 || e.keyCode > 105)) || e.keyCode == 61) {
+            e.preventDefault();
+        }
+    });
+});
