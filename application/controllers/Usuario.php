d70f8c2f0e54734738eb375b41211cb7a1780356
Ivan Mera <imera92@gmail.com>
2017-08-02 03:44:42 -0500

    Ca mbios en vaidaciones para registro de usuario

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -59,7 +59,7 @@ class Usuario extends CI_Controller {
     if($this->input->post('submit')){
       $this->form_validation->set_rules('usuario', 'Usuario', 'required|callback_user_check');
       $this->form_validation->set_rules('nombre', 'Nombre', 'required');
-      $this->form_validation->set_rules('apellido', 'Apellido', 'required');
+      // $this->form_validation->set_rules('apellido', 'Apellido', 'required');
       $this->form_validation->set_rules('correo', 'Email', 'required|valid_email|callback_email_check');
       $this->form_validation->set_rules('clave', 'password', 'required');
       $this->form_validation->set_rules('conf_clave', 'confirmar clave', 'required|matches[clave]');
@@ -86,15 +86,39 @@ class Usuario extends CI_Controller {
           $nuevoCarrito->guardarNuevoCarrito();
         }
 
+        // Borramos exceso de espacios vacios en el username
+        $userInput = $this->input->post('usuario');
+        $userInput = trim($userInput);
+        $userInput = preg_replace('/\s+/', '', $userInput);
+
+        // Borramos exceso de espacios vacios en el nombre
+        $nombreInput = strip_tags($this->input->post('nombre'));
+        $nombreInput = trim($nombreInput);
+        $nombreInput = preg_replace('/\s+/', ' ', $nombreInput);
+
+        // Borramos exceso de espacios vacios en la direccion
+        $direccionInput = strip_tags($this->input->post('direccion'));
+        $direccionInput = trim($direccionInput);
+        $direccionInput = preg_replace('/\s+/', ' ', $direccionInput);
+
+        // Verificamos si se envia apellido y borramos exceso de espacios vacios
+        if (!is_null($this->input->post('apellido'))) {
+          $apellidoInput = strip_tags($this->input->post('apellido'));
+          $apellidoInput = trim($apellidoInput);
+          $apellidoInput = preg_replace('/\s+/', ' ', $apellidoInput);
+        } else {
+          $apellidoInput = '';
+        }
+
         $userData = array(
-          'user' => $this->input->post('usuario'),
-          'nombre' => strip_tags($this->input->post('nombre')),
-          'apellido' => strip_tags($this->input->post('apellido')),
+          'user' => $userInput,
+          'nombre' => $nombreInput,
+          'apellido' => $apellidoInput,
           'email' => strip_tags($this->input->post('correo')),
           'password' => md5($this->input->post('clave')),
           'cedula' => strip_tags($this->input->post('cedula')),
           // 'pais' => strip_tags($this->input->post('pais')),
-          'direccion' => strip_tags($this->input->post('direccion')),
+          'direccion' => $direccionInput,
           'telefono' => strip_tags($this->input->post('telefono')),
           'carrito' => $nuevoCarrito->getId()
         );
--- public/js/crearUsuario.js
+++ public/js/crearUsuario.js
@@ -1,46 +1,70 @@
 $('#userForm').validate({
     debug: false,
     rules:{
+        apellido: {
+            required: false
+        },
+        clave:{
+            minlength: 6
+        },
         conf_clave: {
             equalTo: '#clave'
         },
         telefono:{
             digits: true,
-            minlength: 7,
-            maxlength: 10
+            checkTelefono: true
         },
         cedula:{
             digits: true,
-            minlength: 10,
-            maxlength: 13
+            checkCedula: true
+        },
+        correo:{
+            checkEmail: true
         }
     },
     messages:{
         usuario: 'Ingrese un usuario',
-        nombre: 'Ingrese un nombre',
-        apellido: 'Ingrese un apellido',
+        nombre: 'Ingrese al menos un nombre o su razón social',
         correo: {
             required:'Ingrese un correo',
             email: 'Ingrese un correo válido'
         },
-        clave: 'Ingrese su contraseña',
-        conf_clave: {
-            required: 'Confirme su contraseña',
-            equalTo: 'La contraseña no coincide'
+        clave: {
+            required: 'Ingrese una contraseña',
+            minlength: 'Su contraseña debe tener un mínimo de 6 caracteres'
         },
-        cedula: 'Ingrese una cedula o RUC válido',
+        conf_clave: {
+            required: 'Es necesario que confirme su contraseña',
+            equalTo: 'Las contraseñas no coinciden'
+        },
+        cedula: {
+            required: 'Ingrese un número de cédula o RUC',
+            checkCedula: 'Su número de cédula o RUC debe tener un mínimo de 10 dígitos',
+            digits: 'Sólo puede ingresar dígitos en este campo'
+        },
         telefono: {
             required:'Ingrese un télefono',
-            digits: 'Ingrese un télefono válido'
+            checkTelefono: 'Ingrese un télefono válido (042123456/0981234567)',
+            digits: 'Sólo puede ingresar dígitos en este campo'
         },
         direccion: 'Ingrese su dirección'
     }
 });
 
+$.validator.addMethod('checkEmail', function(value, element) {
+    return this.optional(element) || /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+}, 'Ingrese una dirección de correo válida');
+$.validator.addMethod('checkCedula', function(value, element) {
+    return this.optional(element) || value.length == 10 || value.length == 13;
+}, 'Ingrese un número de cédula o RUC válido');
+$.validator.addMethod('checkTelefono', function(value, element) {
+    return this.optional(element) || value.length == 9 || value.length == 10;
+}, 'Ingrese un número de teléfono válido');
+
 $(document).ready(function() {
     $("input[name='usuario']").keydown(function (e) {
         // Permitir: backspace, delete, tab, escape, enter, guion y .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 173, 190]) !== -1 ||
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 173, 189, 190]) !== -1 ||
             // Permitir: Ctrl/cmd+A
             (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: Ctrl/cmd+C
@@ -51,7 +75,7 @@ $(document).ready(function() {
             (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: F5
             (e.keyCode == 116) ||
-             // Permitir: home, end, left, right
+             // Permitir: home, end, left, up, right
             (e.keyCode >= 35 && e.keyCode <= 39)) {
                  // No hacer nada, dejar que las teclas puedan se presionadas
                  return;
@@ -60,9 +84,9 @@ $(document).ready(function() {
             e.preventDefault();
         }
     });
-    $("input[name='nombre'], input[name='apellido']").keydown(function (e) {
-        // Permitir: backspace, delete, tab, escape, enter
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+    $("input[name='correo']").keydown(function (e) {
+        // Permitir: backspace, delete, tab, escape, enter, guion y .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 189, 190]) !== -1 ||
             // Permitir: Ctrl/cmd+A
             (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: Ctrl/cmd+C
@@ -73,7 +97,29 @@ $(document).ready(function() {
             (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: F5
             (e.keyCode == 116) ||
-             // Permitir: home, end, left, right
+             // Permitir: home, end, left, up, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // No hacer nada, dejar que las teclas puedan se presionadas
+                 return;
+        }
+        if (((e.keyCode < 48 || e.keyCode > 90) && (e.keyCode < 96 || e.keyCode > 105)) || e.keyCode == 61) {
+            e.preventDefault();
+        }
+    });
+    $("input[name='nombre'], input[name='apellido']").keydown(function (e) {
+        // Permitir: space, backspace, delete, tab, escape, enter
+        if ($.inArray(e.keyCode, [32, 46, 8, 9, 27, 13, 110]) !== -1 ||
+            // Permitir: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: Ctrl/cmd+F5
+            (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Permitir: F5
+            (e.keyCode == 116) ||
+             // Permitir: home, end, left, up, right
             (e.keyCode >= 35 && e.keyCode <= 39) ||
             // Permitir: ñ
             (e.keyCode == 0)) {
@@ -97,18 +143,19 @@ $(document).ready(function() {
             (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: F5
             (e.keyCode == 116) ||
-             // Permitir: home, end, left, right
+             // Permitir: home, end, left, up, right
             (e.keyCode >= 35 && e.keyCode <= 39)) {
                  // No hacer nada, dejar que las teclas puedan se presionadas
                  return;
         }
-        if (e.shiftKey || ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) || e.keyCode == 61) {
+        if (e.shiftKey || ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) || e.keyCode == 61 || e.keyCode == 0 || e.keyCode == 191) {
             e.preventDefault();
+            console.log('ok');
         }
     });
     $("input[name='direccion']").keydown(function (e) {
         // Permitir: space, backspace, delete, tab, escape, enter, guion y .
-        if ($.inArray(e.keyCode, [32, 46, 8, 9, 27, 13, 110, 173, 190]) !== -1 ||
+        if ($.inArray(e.keyCode, [32, 46, 8, 9, 27, 13, 110, 189, 190]) !== -1 ||
             // Permitir: Ctrl/cmd+A
             (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: Ctrl/cmd+C
@@ -119,7 +166,7 @@ $(document).ready(function() {
             (e.keyCode == 116 && (e.ctrlKey === true || e.metaKey === true)) ||
             // Permitir: F5
             (e.keyCode == 116) ||
-             // Permitir: home, end, left, right
+             // Permitir: home, end, left, up, right
             (e.keyCode >= 35 && e.keyCode <= 39)) {
                  // No hacer nada, dejar que las teclas puedan se presionadas
                  return;
