19b145b1ba3c6b4aded9cdf2a7762e32d4c487a0
Juan Alvarado <juaelalv@espol.edu.ec>
2017-08-14 15:54:41 -0500

    Sprint 4 task # 101

---
+++ application/controllers/api/Cita.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Cita extends REST_Controller{
+	function _construct(){
+		parent::__construct();
+	}
+
+	public function index_post{
+
+	}
+
+	public function index_get{
+
+	}
+
+	public function index_put{
+
+	}
+
+}
