39b976d363c7d902116cb80718a0831c3f65c2a7
Juan Alvarado <juaelalv@espol.edu.ec>
2017-08-14 15:54:02 -0500

    Sprint 4 task #98

---
+++ application/controllers/api/User.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class User extends REST_Controller{
+	function _construct(){
+		parent::__construct();
+	}
+
+	public function index_post{
+
+	}
+
+}
